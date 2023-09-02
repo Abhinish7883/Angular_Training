70cf3fc2a7f217d933e1c389c54c05b5be9b5688
Abhinish Tiwari <84923349+Abhinish7883@users.noreply.github.com>
2023-09-02 14:34:05 +0530

    Add files via upload
    vimal sir task

---
+++ viewchild_viewchilden/practice/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ChildcompComponent } from './component/childcomp/childcomp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildcompComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
